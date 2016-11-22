7bb7297c1e3100b9cae5f088fb9e7cfdb658448b
Frank Mullen <frankmullen@gmail.com>
2016-11-22 16:14:05 +0000

    'map logic'

--- adventure.py
+++ adventure.py
@@ -21,9 +21,15 @@ while True:
             print 'to the %s is a %s' % (k, possible_location)
             valid_directions[k] = possible_position
 
+    # print map
+    for k, v in valid_directions.iteritems():
+        print locations.get(v)
+
+
     direction = raw_input('which direction do you want to go?\n')
     new_position = valid_directions.get(direction)
     if new_position:
         position = new_position
     else:
         print "sorry, that isn't a valid direction"
+
